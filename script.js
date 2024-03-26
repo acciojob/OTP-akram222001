d5cdadf81e645f23545f3de84a5c96bf6efff9f6
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-03-27 00:20:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener('keydown', (e) => {
+        if (e.keyCode === 8 && index > 0) {
+          inputs[index - 1].focus();
+        }
+      });
+    });
